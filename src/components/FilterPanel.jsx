4743622f0f33fbb6aa8797c621b007992e86fb0d
лу <etosdelaljamal@gmail.com>
2025-03-26 15:20:16 +0300

    filterpanel

--- src/components/FilterPanel.jsx
+++ src/components/FilterPanel.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, ButtonGroup } from '@mui/material';
 const FilterPanel = ({ onFilterChange }) => {
- const categories = ['Все', 'electronics', 'clothing', 'books'];
+ const categories = ['Все', 'электроника', 'одежда', 'книги'];
  return (
  <ButtonGroup variant="contained" sx={{ my: 2 }}>
  {categories.map((category) => (
